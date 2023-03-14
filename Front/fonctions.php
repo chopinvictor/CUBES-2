15a2eff881ceb70b989305d5043594f152d0036f
Jaggerlobster <121859603+Jaggerlobster@users.noreply.github.com>
2023-03-14 12:12:56 +0100

    rajout requete 3 dernière valeur

--- Front/fonctions.php
+++ Front/fonctions.php
@@ -73,3 +73,55 @@ function getLastHeureMaison(){
     $r = $db->query("SELECT date_heure FROM meteo WHERE referencement = 'opm' ORDER BY date_heure DESC LIMIT 1;");
     return $r->fetchColumn();
 }
+
+
+//Toutes les requetes qui vont récuperer les 3 dernière donnée en excluant la dernière de openweathermap
+function getLastTemperature3(){
+    $db = getDb();
+    $r = $db->query("SELECT temperature FROM meteo WHERE referencement = 'opm' ORDER BY date_heure DESC LIMIT 1, 3;");
+    return $r->fetchColumn();
+}
+
+function getLastHumidite3(){
+    $db = getDb();
+    $r = $db->query("SELECT humidite FROM meteo WHERE referencement = 'opm' ORDER BY date_heure DESC LIMIT 1, 3;");
+    return $r->fetchColumn();
+}
+
+function getLastPression3(){
+    $db = getDb();
+    $r = $db->query("SELECT pression FROM meteo WHERE referencement = 'opm' ORDER BY date_heure DESC LIMIT 1, 3;");
+    return $r->fetchColumn();
+}
+
+function getLastHeure3(){
+    $db = getDb();
+    $r = $db->query("SELECT date_heure FROM meteo WHERE referencement = 'opm' ORDER BY date_heure DESC LIMIT 1, 3;");
+    return $r->fetchColumn();
+}
+
+
+//Toutes les requetes qui vont récuperer les 3 dernière donnée en excluant la dernière de Maison
+function getLastTemperatureMaison3(){
+    $db = getDb();
+    $r = $db->query("SELECT temperature FROM meteo WHERE referencement = 'sen' ORDER BY date_heure DESC LIMIT 1, 3;");
+    return $r->fetchColumn();
+}
+
+function getLastHumiditeMaison3(){
+    $db = getDb();
+    $r = $db->query("SELECT humidite FROM meteo WHERE referencement = 'sen' ORDER BY date_heure DESC LIMIT 1, 3;");
+    return $r->fetchColumn();
+}
+
+function getLastPressionMaison3(){
+    $db = getDb();
+    $r = $db->query("SELECT pression FROM meteo WHERE referencement = 'sen' ORDER BY date_heure DESC LIMIT 1, 3;");
+    return $r->fetchColumn();
+}
+
+function getLastHeureMaison3(){
+    $db = getDb();
+    $r = $db->query("SELECT date_heure FROM meteo WHERE referencement = 'sen' ORDER BY date_heure DESC LIMIT 1, 3;");
+    return $r->fetchColumn();
+}
